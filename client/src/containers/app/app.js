fbb98c4dd836281b0315503945439df22f40f29c
Lukas Kybartas <lukask@wix.com>
2019-05-07 16:31:56 +0300

    [client]add redux

--- client/src/containers/app/app.js
+++ client/src/containers/app/app.js
@@ -1,15 +1,21 @@
 import React, { Component, Fragment } from 'react';
-import {CssBaseline} from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
+import { Routes } from '../routes';
+import { Provider } from 'react-redux';
+import reducers from '../../reducers';
+import { createStore } from 'redux';
 
-import {Routes} from '../routes';
+const store = createStore(reducers, {});
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <CssBaseline />
-        <Routes/>
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <CssBaseline />
+          <Routes />
+        </Fragment>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import userData from './user-data';
+
+export default combineReducers({ userData });
