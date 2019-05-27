d9efb31219c18e06d9c46975cf4cb8a38a131ed0
Lukas Kybartas <lukask@wix.com>
2019-05-27 07:49:12 +0300

    [client]add login view

---
+++ client/src/actions/login-user.js
@@ -0,0 +1,26 @@
+import { createAction } from 'redux-actions';
+import axios from 'axios';
+import { apiBaseUrl } from '../constants/env';
+
+export const LOGIN_USER_REQUEST = 'loginUser/FETCH_REQUEST';
+export const LOGIN_USER_SUCCESS = 'loginUser/FETCH_SUCCESS';
+export const LOGIN_USER_FAILURE = 'loginUser/FETCH_FAILURE';
+
+export const loginUserRequest = createAction(LOGIN_USER_REQUEST);
+export const loginUserSuccess = createAction(LOGIN_USER_SUCCESS);
+export const loginUserFailure = createAction(LOGIN_USER_FAILURE);
+
+export const loginUser = (userName, password) => {
+  return async dispatch => {
+    dispatch(loginUserRequest());
+    try {
+      //TODO: create a wrapper for requests which includes the cookie!
+      //TODO: update url
+      //TODO: send the userName and password
+      const result = await axios.post(`${apiBaseUrl}/`);
+      dispatch(loginUserSuccess(result.data));
+    } catch {
+      dispatch(loginUserFailure());
+    }
+  };
+};
--- client/src/containers/routes/routes.js
+++ client/src/containers/routes/routes.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { TripsView } from '../trips-view';
 import * as routes from '../../constants/routes';
 import { View } from '../view';
 import TripForm from '../trip-form/trip-form';
+import { LoginView } from '../login-view';
+import { connect } from 'react-redux';
+import { isLoggedIn } from '../../selectors/user-data';
 
-const Routes = () => (
+const Routes = ({ isLoggedIn }) => (
   <Router>
     <Route
       path={routes.MY_TRIPS_ROUTE}
       exact
-      render={props => (
-        <View active={routes.MY_TRIPS_ROUTE}>
-          <TripsView {...props} />
-        </View>
-      )}
+      render={props =>
+        isLoggedIn ? (
+          <View active={routes.MY_TRIPS_ROUTE}>
+            <TripsView {...props} />
+          </View>
+        ) : (
+          <LoginView />
+        )
+      }
     />
     <Route
       path={routes.ALL_TRIPS_ROUTE}
@@ -37,4 +45,12 @@ const Routes = () => (
   </Router>
 );
 
-export default Routes;
+Routes.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  isLoggedIn: isLoggedIn(state)
+});
+
+export default connect(mapStateToProps)(Routes);
