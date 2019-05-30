44f0251268d41da01c15ca8bc693453aa07d167c
Lukas Kybartas <lukask@wix.com>
2019-05-30 12:20:52 +0300

    [client]take userType from login response

--- client/src/reducers/user-data.js
+++ client/src/reducers/user-data.js
@@ -1,12 +1,14 @@
-//TODO: fix hard-coded userData! Implement real login!
 import { LOGIN_USER_SUCCESS } from '../actions/login-user';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case LOGIN_USER_SUCCESS:
+      const {token, userType} = action.payload;
       return {
         ...state,
-        token: action.payload.token,
+        token,
+        userType,
+        userId: action.payload.userId, //TODO: remove this line and all usages of userId!
         isLoggedIn: true
       };
     default:
