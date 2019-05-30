455b6ef71e0a0ce7707c36fd556a4be07433ff06
Lukas Kybartas <lukask@wix.com>
2019-05-30 12:09:34 +0300

    [client]fix request-helpers

--- client/src/helpers/request-helpers.js
+++ client/src/helpers/request-helpers.js
@@ -3,11 +3,11 @@ import { apiBaseUrl } from '../constants/env';
 
 export const requestConfig = {};
 
-export const get = path => axios.get(`${apiBaseUrl}/${path}`);
+export const get = path => axios.get(`${apiBaseUrl}${path}`);
 export const post = (path, options) => {
   options = {
     ...options,
     ...(requestConfig.token && { token: requestConfig.token })
   };
-  return axios.post(`${apiBaseUrl}/${path}`, options);
+  return axios.post(`${apiBaseUrl}${path}`, options);
 };
